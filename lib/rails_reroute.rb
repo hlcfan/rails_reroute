0d4397d1fc86465edca5c56cae2b5a0ff2ea80fa
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-03-14 15:06:27 -0700

    Refactored to work on multiple servers

--- lib/rails_reroute.rb
+++ lib/rails_reroute.rb
@@ -3,24 +3,32 @@ module RailsReroute
     initializer "rails_reroute.initializer" do
       ActionController::Base.class_eval do
         def reroute new_route
-          new_paths = {"PATH_INFO" => "#{new_route}", "REQUEST_URI"=>"#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{new_route}", "REQUEST_PATH"=>"#{new_route}"}
-          env.keys.each_with_index do |key,idx|
-            env.delete(key) if idx > 29
-          end
-          env.merge! new_paths
+          new_env = reroute_new_env new_route
           unlock_request
-          Rails.application.call(env)
-          @_response = env["action_controller.instance"].response
+          Rails.application.call(new_env)
+          @_response = new_env["action_controller.instance"].response
           @_response_body = @_response.body
         end
 
+        def reroute_default_variables
+          %w( CONTENT_TYPE CONTENT_LENGTH
+              HTTPS AUTH_TYPE GATEWAY_INTERFACE
+              PATH_INFO PATH_TRANSLATED QUERY_STRING
+              REMOTE_ADDR REMOTE_HOST REMOTE_IDENT REMOTE_USER
+              REQUEST_METHOD SCRIPT_NAME
+              SERVER_NAME SERVER_PORT SERVER_PROTOCOL SERVER_SOFTWARE )
+        end
+
+        def reroute_new_env new_route
+          new_env = env.clone
+          new_paths = {"PATH_INFO" => "#{new_route}", "REQUEST_URI"=>"#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{new_route}", "REQUEST_PATH"=>"#{new_route}"}
+          reroute_default_variables.each { |key| new_env[key] = env[key] }
+          new_env.merge new_paths
+        end
+
         def unlock_request
-          Rails.application.app.instance_eval do
-            if @app.kind_of? Rack::Lock
-              @app.instance_eval do
-                @mutex.unlock
-              end
-            end
+          unless Rails.application.config.allow_concurrency
+            Rack::Lock.new(Rails.application).call(env) rescue ThreadError
           end
         end
       end
